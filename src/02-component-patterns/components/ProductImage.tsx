0671a0ec4a4e1259b72157181bf981a369516a03
yoGhastly <ve06976@innovaccion.mx>
2022-06-18 15:25:22 -0500

    Component Compound ready

---
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -0,0 +1,11 @@
+import { useContext } from 'react';
+import { ProductContext } from './ProductCard';
+import noImage from '../assets/no-image.jpg';
+import styles from '../styles/styles.module.css';
+
+export const ProductImage = ({ img = '' }) => {
+	const { product } = useContext(ProductContext);
+	let imgToShow = img || product.img || noImage;
+
+	return <img className={styles.productImg} src={imgToShow} alt="Product" />;
+};
